7a61d43289b439268939953b97aefa1ce9e086dc
neelfirst <neelfirst@gmail.com>
2021-08-03 20:22:06 -0700

    demo program commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app == Flask(__name__)
+
+@app.route("/")
+def hello():
+  return 'hello world'
+
+@app.route("/<string:name>/")
+def say_hello(name):
+  return f"hello {name}"
+
+if __name__ == "__main__":
+  app.run()
